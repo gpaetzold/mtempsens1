5f16de92c6e23e6e7b5e2a4e03a3618054ff5d5d
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-11-03 17:33:48 +0100

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
+let temperatur = 0
 basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+basic.clearScreen()
 basic.forever(function () {
-	
+    temperatur = pins.analogReadPin(AnalogPin.P3)
+    basic.showNumber(temperatur)
 })
