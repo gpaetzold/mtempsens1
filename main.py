87915c37f1cfe8ee66fc5a315ca9363b6f989cef
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-11-04 10:27:40 +0100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-basic.showIcon(IconNames.Happy)
-basic.forever(function () {
-	
-})
+temperature = 0
+basic.show_icon(IconNames.HAPPY)
+basic.pause(1000)
+basic.clear_screen()
+
+def on_forever():
+    global temperature
+    temperature = pins.analog_read_pin(AnalogPin.P3)
+    basic.show_number((temperature - (temperature % 5)) / 5)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
-let temperatur = 0
+let temperature = 0
 basic.showIcon(IconNames.Happy)
 basic.pause(1000)
 basic.clearScreen()
 basic.forever(function () {
-    temperatur = pins.analogReadPin(AnalogPin.P3)
-    basic.showNumber(temperatur)
+    temperature = pins.analogReadPin(AnalogPin.P2)
+    basic.showNumber((temperature - temperature % 5) / 5)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Yes)
+    basic.pause(1000)
 })
